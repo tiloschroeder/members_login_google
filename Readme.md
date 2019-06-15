57ed5d5e142c204ed45f73fa52481d29767c185c
tiloschroeder <tilo.schroeder@bluesky-systems.org>
2019-06-15 11:33:20 +0200

    Initial release

---
+++ Readme.md
@@ -0,0 +1,113 @@
+# Members: Google Login 
+
+> Logs in users using Google Log in
+
+## Specs
+
+This extension creates a user account with the data from a Google user account and automatically logs in the user.
+
+The following user information are supported:
+
+- users email
+- users firstname
+- users lastname
+- username (build from the users name)
+- registered since
+
+## Requirements
+
+- Symphony CMS version 2.7.x
+- Members extension version 1.9.0
+
+## Installation
+
+- `git clone` or download and unpack
+- Upload the extension into the extension directory
+- Enable/install it just like any other extension
+
+## How to use in 3 steps
+
+### 1. Create a new Member section
+
+Create a new Member section with the following fields:
+
+- Member: Email (required) <br /><em>make sure, the handle of the field is `email`</em>
+- Text Input for username (optional)
+- Text Input for firstname (optional)
+- Text Input for lastname (optional)
+- Date Field for 'registered since' (optional)
+
+Go to 'System' -> 'Preferences' and add the Member section to the 'Active Members Section'.
+
+After this open the `config.php` and add the required configuration values:
+
+```php
+###### MEMBERS_GOOGLE_LOGIN ######
+'members_google_login' => array(
+    'client-id' => 'REPLACE ME',
+    'client-secret' => 'REPLACE ME',
+    'client-redirect-url' => 'https://example.org/google/', // The redirect URI (see next step)
+    'members-section-id' => 5,         // ID of the above created Member section
+    'member-username-field' => '10',   // Field ID (optional)
+    'member-firstname-field' => '11',  // Field ID (optional)
+    'member-lastname-field' => '12',   // Field ID (optional)
+    'member-registered-since' => '13', // Field ID (optional)
+),
+########
+```
+
+### 2. Create a new page (Redirect URI)
+
+Create a new page (e.g. "Google") and attach the event __Members: Google Login__ on it.
+
+Add the following form to the new page to handle the log in process when the user will successfully redirected from Google:
+
+```xslt
+<xsl:if test="string-length(/data/params/url-code) != 0">
+    <form id="googleform" method="POST" action="{$current-url}/">
+        <input type="hidden" name="code" value="{/data/params/url-code}" />
+        <button>Validate</button>
+    </form>
+    <script>if (window.googleform) googleform.submit();</script>
+</xsl:if>
+```
+
+Add the following login form to a desired page (e.g. the home page):
+
+```xslt
+<form action="{$root}/google/" method="POST">
+    <input type="hidden" name="redirect" value="{$root}" />
+    <input type="hidden" name="member-google-action[login]" value="Login" />
+    <button>Log in with Google</button>
+</form>
+```
+
+This form will redirect to the new page /google/ and the user will see the Google dialog and then Google will redirect the user to the 'Redirect URI' value.
+
+If everything works, the user will be redirected to the 'redirect' value, just like the standard Members login (e.g. Log in via form).
+
+### 3. Create a Google API Console Project
+
+Go to the (Google API Console)[https://console.developers.google.com/].
+
+Create a __New Project__:
+
+- Enter the __Project Name__
+- Click the __Create__ button
+
+Select __Credentials__ under the __APIs & Services__ section in the left side navigation panel.
+
+Select the __OAuth consent screen__ tab:
+
+- Enter the name of your Application
+- Choose an email address for user support
+- Enter in __Authorized domains__ the domain(s), which will be allowed to authenticate using OAuth
+- Click the __Save__ button
+
+Select the __Credentials__ tab, click the __Create credentials__ drop-down menu and select __OAuth client ID__:
+
+- Select __Web application__
+- Enter the above created page in __Authorized redirect URIs__ field (e.g. https://example.org/google/)
+- Click the __Create__ button
+
+## See a working demo [here](https://login.bluesky-systems.eu)
---
+++ events/event.members_google_login.php
@@ -0,0 +1,200 @@
+<?php
+
+require_once(TOOLKIT . '/class.event.php');
+require_once(EXTENSIONS . '/members_login_google/extension.driver.php');
+
+class eventmembers_google_login extends Event
+{
+    public static function about()
+    {
+        return array(
+            'name' => extension_members_login_google::EXT_NAME,
+            'author' => array(
+                'name' => 'Tilo Schröder',
+                'website' => 'https://bluesky-systems.de',
+                'email' => 'info@bluesky-systems.de',
+            ),
+            'version' => '1.0.0',
+            'release-date' => '2019-06-12T12:15:00+00:00',
+            'trigger-condition' => 'member-google-action[login]',
+        );
+    }
+
+    public function priority()
+    {
+        return self::kHIGH;
+    }
+
+    public static function getSource()
+    {
+        return extension_members_login_google::EXT_NAME;
+    }
+
+    public static function allowEditorToParse()
+    {
+        return false;
+    }
+
+    public function load()
+    {
+        try {
+            $this->trigger();
+        } catch (Exception $ex) {
+            if (Symphony::Log()) {
+                Symphony::Log()->pushExceptionToLog($ex, true);
+            }
+        }
+    }
+
+    public function trigger()
+    {
+        $CLIENT_ID = Symphony::Configuration()->get('client-id', 'members_google_login');
+        $CLIENT_SECRET = Symphony::Configuration()->get('client-secret', 'members_google_login');
+        $CLIENT_REDIRECT_URL = Symphony::Configuration()->get('client-redirect-url', 'members_google_login');
+        $MEMBERS_SECTION_ID = Symphony::Configuration()->get('members-section-id', 'members_google_login');
+
+        if(is_array($_POST['member-google-action']) && isset($_POST['member-google-action']['login'])) {
+            $USER_INFO = urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email');
+            $_SESSION['OAUTH_SERVICE'] = 'google';
+            $_SESSION['OAUTH_START_URL'] = $_REQUEST['redirect'];
+            $_SESSION['OAUTH_CALLBACK_URL'] = $CLIENT_REDIRECT_URL;
+            $_SESSION['OAUTH_MEMBERS_SECTION_ID'] = $MEMBERS_SECTION_ID;
+            $_SESSION['OAUTH_TOKEN'] = null;
+
+            $url = "https://accounts.google.com/o/oauth2/auth?scope=$USER_INFO&redirect_uri=$CLIENT_REDIRECT_URL&response_type=code&client_id=$CLIENT_ID&access_type=online";
+            redirect($url);
+
+        } elseif (isset($_POST['code'])) {
+            $gdata = $this->getAccessToken($CLIENT_ID, $CLIENT_REDIRECT_URL, $CLIENT_SECRET, $_POST['code']);
+
+            if (isset($gdata) && is_array($gdata) === true) {
+                $_SESSION['ACCESS_TOKEN'] = $gdata['access_token'];
+                $user_info = $this->getUserProfileInfo($gdata['access_token']);
+
+                if (is_array($user_info) && isset($user_info['email'])) {
+                    $_SESSION['OAUTH_TIMESTAMP'] = time();
+                    $_SESSION['OAUTH_SERVICE'] = 'google';
+                    $_SESSION['ACCESS_TOKEN_SECRET'] = null;
+                    $_SESSION['OAUTH_USER_ID'] = $user_info['id'];
+                    $_SESSION['OAUTH_USER_NAME'] = $user_info['name'];
+                    $_SESSION['OAUTH_USER_IMG'] =  $user_info['picture'];
+                    $_SESSION['OAUTH_USER_CITY'] = $user_info['locale'];
+                    $_SESSION['OAUTH_USER_EMAIL'] = $user_info['email'];
+
+                    $edriver = Symphony::ExtensionManager()->create('members');
+                    $edriver->setMembersSection($_SESSION['OAUTH_MEMBERS_SECTION_ID']);
+                    $femail = $edriver->getField('email');
+                    $mdriver = $edriver->getMemberDriver();
+                    $email = $user_info['email'];
+                    $m = $femail->fetchMemberIDBy($email);
+
+                    if (!$m) {
+                        $m = new Entry();
+                        $m->set('section_id', $_SESSION['OAUTH_MEMBERS_SECTION_ID']);
+                        $m->setData($femail->get('id'), array('value' => $email));
+                        $memberUsername = Symphony::Configuration()->get('member-username-field', 'members_google_login');
+                        if ($memberUsername and !empty($user_info['name'])) {
+                            $m->setData(General::intval($memberUsername), array(
+                                'value' => strtolower(str_replace(' ', '.', $user_info['name'])),
+                            ));
+                        }
+                        $memberFirstname = Symphony::Configuration()->get('member-firstname-field', 'members_google_login');
+                        if ($memberFirstname and !empty($user_info['given_name'])) {
+                            $m->setData(General::intval($memberFirstname), array(
+                                'value' => $user_info['given_name'],
+                            ));
+                        }
+                        $memberLastname = Symphony::Configuration()->get('member-lastname-field', 'members_google_login');
+                        if ($memberLastname and !empty($user_info['family_name'])) {
+                            $m->setData(General::intval($memberLastname), array(
+                                'value' => $user_info['family_name'],
+                            ));
+                        }
+                        $memberSince = Symphony::Configuration()->get('member-registered-since', 'members_google_login');
+                        if ($memberSince) {
+                            $today = $this->_env['param']['today'];
+                            $time = $this->_env['param']['current-time'];
+                            $m->setData(General::intval($memberSince), array(
+                                'value' => $today . ' ' . $time,
+                            ));
+                        }
+                        $m->commit();
+                        $m = $m->get('id');
+                    }
+
+                    $_SESSION['OAUTH_MEMBER_ID'] = $m;
+                    $login = $mdriver->login(array(
+                        'email' => $email
+                    ));
+
+                    if ($login) {
+                        redirect($_SESSION['OAUTH_START_URL']);
+                    } else  {
+                        throw new Exception('Google login failed');
+                    }
+                } else {
+                    $_SESSION['OAUTH_SERVICE'] = null;
+                    $_SESSION['ACCESS_TOKEN'] = null;
+                    $_SESSION['OAUTH_TIMESTAMP'] = 0;
+                    session_destroy();
+                }
+            } else {
+                $_SESSION['OAUTH_SERVICE'] = null;
+                $_SESSION['OAUTH_START_URL'] = null;
+                $_SESSION['OAUTH_MEMBERS_SECTION_ID'] = null;
+                $_SESSION['OAUTH_TOKEN'] = null;
+                session_destroy();
+            }
+        } elseif (is_array($_POST['member-google-action']) && isset($_POST['member-google-action']['logout']) ||
+                  is_array($_POST['member-action']) && isset($_POST['member-action']['logout'])) {
+            $_SESSION['OAUTH_SERVICE'] = null;
+            $_SESSION['OAUTH_START_URL'] = null;
+            $_SESSION['OAUTH_MEMBERS_SECTION_ID'] = null;
+            $_SESSION['OAUTH_TOKEN'] = null;
+            session_destroy();
+        }
+    }
+
+    private function getAccessToken($client_id, $redirect_uri, $client_secret, $code)
+    {
+        $url = 'https://www.googleapis.com/oauth2/v4/token';
+
+        $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=' . $client_secret . '&code='. $code . '&grant_type=authorization_code';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = json_decode(curl_exec($ch), true);
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+
+        if($http_code !== 200) {
+            throw new Exception('Error : Failed to recieve access token');
+        }
+
+        return $data;
+    }
+
+    private function getUserProfileInfo($access_token) 
+    {
+        // See: https://any-api.com/googleapis_com/oauth2/docs/userinfo/oauth2_userinfo_get
+        $url = 'https://www.googleapis.com/oauth2/v2/userinfo?fields=name,given_name,family_name,email,id,locale,picture,verified_email';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token));
+        $data = json_decode(curl_exec($ch), true);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if($http_code !== 200) {
+            throw new Exception('Error : Failed to get user information');
+        }
+
+        return $data;
+    }
+
+}
